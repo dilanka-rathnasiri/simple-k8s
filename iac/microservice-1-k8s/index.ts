e98aa1efa1b0363ccc97b1ec426a6bc6335aaee2
dilanka-rathnasiri <rdilanka17@gmail.com>
2024-07-13 09:52:55 +0530

    complete microservice 1 iac

--- iac/microservice-1-k8s/index.ts
+++ iac/microservice-1-k8s/index.ts
@@ -1,20 +1,36 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const ms1AppLabels = { app: "microservice-1" };
-const ms1Deployment = new k8s.apps.v1.Deployment("microservice-1", {
-    spec: {
-        selector: { matchLabels: ms1AppLabels },
-        replicas: 1,
-        template: {
-            metadata: { labels: ms1AppLabels },
-            spec: {
-                containers: [{
-                    name: "microservice-1",
-                    image: "dilankarathnasiri/spring-k8s:microservice-1",
-                    ports: [{containerPort: 5000}]
-                }]
-            }
-        }
+const ms1AppLabels = {app: "microservice-1"};
+const ms1Deployment = new k8s.apps.v1.Deployment("microservice-1-deployment", {
+  metadata: {
+    name: "microservice-1-deployment"
+  },
+  spec: {
+    selector: {matchLabels: ms1AppLabels},
+    replicas: 1,
+    template: {
+      metadata: {labels: ms1AppLabels},
+      spec: {
+        containers: [{
+          name: "microservice-1",
+          image: "dilankarathnasiri/spring-k8s:microservice-1",
+          ports: [{containerPort: 5000}]
+        }]
+      }
     }
+  }
 });
-export const ms1Name = ms1Deployment.metadata.name;
+
+const ms1Service = new k8s.core.v1.Service("microservice-1-service", {
+  metadata: {
+    name: "microservice-1-service",
+  },
+  spec: {
+    ports: [{
+      port: 80,
+      protocol: "TCP",
+      targetPort: 5000
+    }],
+    selector: ms1AppLabels
+  }
+})
