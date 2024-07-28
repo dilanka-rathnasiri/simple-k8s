9f143232f6acb4fd0bdcff90c24373451e1ef991
dilanka-rathnasiri <rdilanka17@gmail.com>
2024-07-28 22:38:28 +0530

    iac for k8s-dashboard user

--- iac/k8s-dashboard/index.ts
+++ iac/k8s-dashboard/index.ts
@@ -1,21 +1,52 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const dashboardNamespace = new k8s.core.v1.Namespace(
-  "kubernetes-dashboard",
+const nsName: string = "kubernetes-dashboard";
+const dashboardNamespace = new k8s.core.v1.Namespace(nsName,
   {
     metadata: {
-      name: "kubernetes-dashboard",
+      name: nsName,
     },
-  },
-);
-const dashboardChart = new k8s.helm.v4.Chart(
-  "kubernetes-dashboard",
+  },);
+
+const dashboardChart = new k8s.helm.v4.Chart(nsName,
   {
     chart: "kubernetes-dashboard",
-    namespace: "kubernetes-dashboard",
+    namespace: nsName,
     repositoryOpts: {
       repo: "https://kubernetes.github.io/dashboard/",
     },
+  }, {dependsOn: [dashboardNamespace]},);
+
+const usr = new k8s.core.v1.ServiceAccount("usr", {
+  metadata: {
+    name: "monitoring-user",
+    namespace: nsName
+  }
+}, {dependsOn: [dashboardNamespace]});
+
+const usrCrb = new k8s.rbac.v1.ClusterRoleBinding("usr-crb", {
+  roleRef: {
+    apiGroup: "rbac.authorization.k8s.io",
+    kind: "ClusterRole",
+    name: "view"
   },
-  { dependsOn: [dashboardNamespace] },
-);
+  metadata: {
+    name: "monitoring-user"
+  },
+  subjects: [{
+    kind: "ServiceAccount",
+    name: "monitoring-user",
+    namespace: nsName
+  }]
+}, {dependsOn: [dashboardNamespace, usr]},)
+
+const usrSecret = new k8s.core.v1.Secret("usr-secret", {
+  metadata: {
+    name: "monitoring-user",
+    namespace: nsName,
+    annotations: {
+      "kubernetes.io/service-account.name": "monitoring-user"
+    },
+  },
+  type: "kubernetes.io/service-account-token"
+}, {dependsOn: [dashboardNamespace, usr]});
