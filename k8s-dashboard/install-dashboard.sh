cc5b0bab3a1c6c7019b92b7450c3ab4778fec5aa
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-02 14:36:13 +0530

    test

--- k8s-dashboard/install-dashboard.sh
+++ k8s-dashboard/install-dashboard.sh
@@ -8,9 +8,7 @@ helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update
 helm upgrade -i kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
   -n k8s-dashboard \
-  --values values.yaml \
-  --wait \
-  --timeout 5m0s
+  --values values.yaml
 
 echo "******* create admin user *******"
 kubectl apply -f monitoring-user.yaml
