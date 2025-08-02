397795b50d6a84fd69d0536e97d29f4b9c36360d
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-02 14:14:55 +0530

    format script

--- k8s-dashboard/install-dashboard.sh
+++ k8s-dashboard/install-dashboard.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
 
-# Exit on error
-set -e
-
-# Set variables
-NAMESPACE="kubernetes-dashboard"
-HELM_REPO_NAME="kubernetes-dashboard"
-HELM_REPO_URL="https://kubernetes.github.io/dashboard/"
-HELM_CHART="kubernetes-dashboard"
-HELM_RELEASE="kubernetes-dashboard"
-
-# Add the Kubernetes Dashboard Helm repository
-echo "Adding Kubernetes Dashboard Helm repository..."
-helm repo add $HELM_REPO_NAME $HELM_REPO_URL
+echo "******* add kubernetes dashboard helm repository *******"
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update
 
-# Create the namespace if it doesn't exist
-echo "Creating namespace $NAMESPACE if it doesn't exist..."
-kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+echo "******* create dashboard namespace *******"
+kubectl apply -f k8s-dashboard-ns.yaml
 
-# Install or upgrade the Kubernetes Dashboard
-echo "Installing/Upgrading Kubernetes Dashboard..."
-helm upgrade --install $HELM_RELEASE $HELM_REPO_NAME/$HELM_CHART \
-  --namespace $NAMESPACE \
+echo "******* install kubernetes dashboard *******"
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+  --namespace kubernetes-dashboard \
   --values values.yaml \
   --wait \
   --timeout 5m0s
 
-# Create admin user
-echo "Creating admin user..."
-kubectl apply -f create-admin-user.yaml -n $NAMESPACE
+echo "******* create admin user *******"
+kubectl apply -f create-admin-user.yaml -n kubernetes-dashboard
 
-echo "\nKubernetes Dashboard has been installed successfully!"
+echo "******* kubernetes dashboard installed successfully *******"
 echo "To access the dashboard, run: kubectl proxy"
 echo "Then open: http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
-echo "\nUse the token above to log in."
--- k8s-dashboard/uninstall-dashboard.sh
+++ k8s-dashboard/uninstall-dashboard.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+echo "******* uninstall kubernetes dashboard *******"
+helm uninstall -n k8s-dashboard kubernetes-dashboard
 
-# Set variables
-NAMESPACE="kubernetes-dashboard"
-HELM_RELEASE="kubernetes-dashboard"
-
-# Uninstall the Helm release
-echo "Uninstalling Kubernetes Dashboard..."
-if helm list -n $NAMESPACE | grep -q $HELM_RELEASE; then
-    helm uninstall $HELM_RELEASE -n $NAMESPACE
-else
-    echo "Helm release $HELM_RELEASE not found in namespace $NAMESPACE"
-fi
-
-# Optionally, delete the namespace
-# Uncomment the following lines if you want to delete the namespace as well
-# echo "Deleting namespace $NAMESPACE..."
-# kubectl delete namespace $NAMESPACE
-
-echo "Kubernetes Dashboard has been uninstalled successfully!"
-echo "Note: The namespace $NAMESPACE has been preserved. To delete it, uncomment the relevant lines in this script."
+echo "******* delete dashboard namespace *******"
+kubectl delete -f k8s-dashboard-ns.yaml
