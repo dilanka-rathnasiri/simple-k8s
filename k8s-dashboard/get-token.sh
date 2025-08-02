7dcc6b10bd10864ebb2a34af12f22f26babfeda8
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-02 14:25:44 +0530

    test

--- k8s-dashboard/get-token.sh
+++ k8s-dashboard/get-token.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+echo "******* get token *******"
+TOKEN=$(kubectl -n k8s-dashboard create token monitoring-user)
 
-# Set variables
-export NAMESPACE="kubernetes-dashboard"
-export SERVICE_ACCOUNT="monitoring-user"
-
-# Get the token
-TOKEN=$(kubectl -n kubernetes-dashboard create token monitoring-user)
-
-# Display the token
-echo ""
-echo "=================================================="
-echo " TOKEN FOR KUBERNETES DASHBOARD"
-echo "=================================================="
+echo "******* token obtained *******"
 echo $TOKEN
-echo "=================================================="
-echo ""
-echo "Copy this token and use it to log in to the dashboard."
