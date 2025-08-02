1ad4e0adf7d06fd45ec548bdfe441a226bbdf4bc
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-02 14:27:47 +0530

    test

--- k8s-dashboard/install-dashboard.sh
+++ k8s-dashboard/install-dashboard.sh
@@ -6,15 +6,11 @@ kubectl apply -f k8s-dashboard-ns.yaml
 echo "******* install kubernetes dashboard *******"
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm repo update
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
-  --namespace kubernetes-dashboard \
+helm upgrade -i kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+  -n k8s-dashboard \
   --values values.yaml \
   --wait \
   --timeout 5m0s
 
 echo "******* create admin user *******"
-kubectl apply -f create-monitoring-user.yaml -n kubernetes-dashboard
-
-echo "******* kubernetes dashboard installed successfully *******"
-echo "To access the dashboard, run: kubectl proxy"
-echo "Then open: http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+kubectl apply -f monitoring-user.yaml
